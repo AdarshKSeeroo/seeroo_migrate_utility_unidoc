fbc6b1a9d53106e03051f8e6d0e145779fe604f0
AdarshKSeeroo <144668088+AdarshKSeeroo@users.noreply.github.com>
2024-12-19 12:57:50 +0530

    changed export.

--- lib/seeroo_migrate_utility_unidoc.dart
+++ lib/seeroo_migrate_utility_unidoc.dart
@@ -1,4 +1,43 @@
 library seeroo_migrate_utility_unidoc;
 
-export 'package:google_place/google_place.dart';
-export 'package:flutter_google_places/flutter_google_places.dart';
+export 'package:google_place/src/autocomplete/autocomplete_prediction.dart';
+export 'package:google_place/src/autocomplete/autocomplete_response.dart';
+export 'package:google_place/src/details/details_response.dart';
+export 'package:google_place/src/details/details_result.dart';
+export 'package:google_place/src/models/address_component.dart';
+export 'package:google_place/src/models/bounds.dart';
+export 'package:google_place/src/models/close.dart';
+export 'package:google_place/src/models/component.dart';
+export 'package:google_place/src/models/debug_log.dart';
+export 'package:google_place/src/models/geometry.dart';
+export 'package:google_place/src/models/input_type.dart';
+export 'package:google_place/src/models/lat_lon.dart';
+export 'package:google_place/src/models/location.dart';
+export 'package:google_place/src/models/locationbias.dart';
+export 'package:google_place/src/models/main_text_matched_substring.dart';
+export 'package:google_place/src/models/matched_substring.dart';
+export 'package:google_place/src/models/northeast.dart';
+export 'package:google_place/src/models/open.dart';
+export 'package:google_place/src/models/opening_hours.dart';
+export 'package:google_place/src/models/period.dart';
+export 'package:google_place/src/models/photo.dart';
+export 'package:google_place/src/models/plus_code.dart';
+export 'package:google_place/src/models/rank-by.dart';
+export 'package:google_place/src/models/review.dart';
+export 'package:google_place/src/models/southwest.dart';
+export 'package:google_place/src/models/structured_formatting.dart';
+export 'package:google_place/src/models/term.dart';
+export 'package:google_place/src/models/viewport.dart';
+export 'package:google_place/src/search/find_place_response.dart';
+export 'package:google_place/src/search/near_by_search_response.dart';
+export 'package:google_place/src/search/search_candidate.dart';
+export 'package:google_place/src/search/search_result.dart';
+export 'package:google_place/src/search/text_search_response.dart';
+
+export 'src/packages/flutter_google_places/lib/src/flutter_google_places.dart';
+export 'src/packages/flutter_google_places/lib/src/places_autocomplete_field.dart';
+export 'src/packages/flutter_google_places/lib/src/places_autocomplete_form_field.dart';
+export 'src/packages/google_maps_webservice/lib/src/core.dart'
+    hide Location, Geometry, Bounds, AddressComponent, Component;
+export 'src/packages/google_maps_webservice/lib/src/places.dart'
+    hide Photo, StructuredFormatting, Review, Term, MatchedSubstring;
